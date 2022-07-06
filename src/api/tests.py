5f588856d8f5db9e01058f9620713462a598dbca
Влад Костюк <vladkostiuk@MacBook-Air-Vlad.local>
2022-07-06 16:34:06 +0300

    added tests

--- src/api/tests.py
+++ src/api/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from shop.models import Category, Product
+
+
+class TestAPI(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.product = Product.objects.create(
+            title="test", category=Category.objects.create(name="name"), description="TEST"
+        )
+
+    def tearDown(self) -> None:
+        self.product.delete()
+
+    def test_product_list_wrong_user(self):
+        result = self.client.get(reverse("api:product_details", kwargs={"uuid": self.product.uuid}))
+        self.assertEqual(result.status_code, status.HTTP_401_UNAUTHORIZED)
