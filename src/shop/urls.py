afdcfa2e2ffec711b8ce094b311e07f65c8b3e7f
Влад Костюк <vladkostiuk@MacBook-Air-Vlad.local>
2022-08-05 15:38:50 +0300

    Added cart and checkout functionality and fix some bugs

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 from shop import views
 from shop.views import (CategorySelectView, CategoryView,
-                        ProductDetailView, cart, location)
+                        ProductDetailView, cart, location, checkout, add_cart)
 
 app_name = "shop"
 
@@ -13,6 +13,10 @@ urlpatterns = [
     path("categories/", CategoryView.as_view(), name="categories"),
     path("category/<slug:cat_slug>/", CategorySelectView.as_view(), name="category"),
     path("cart/", views.cart, name="cart"),
-    # path("update_item/", views.updateItem, name="update_item"),
+    path("add-cart/<uuid:product>/", views.add_cart, name="add_cart"),
+    path("delet-cart/", views.delete_cart, name="delete_cart"),
+    # path("clear-cart/<uuid:product>/", ClearItemCart.as_view(), name="clear_cart"),
     path("location/", views.location, name="location"),
+    path("checkout/", views.checkout, name="checkout"),
+
 ]
