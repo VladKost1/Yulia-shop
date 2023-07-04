0fe114a1c72f12b52f356e28d9a2e44971b3852a
Vladislav Kostiuk <vladislav.kostiuk@fbt.loc>
2023-07-04 17:30:08 +0300

    Heroku deploy

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os.path
 from datetime import timedelta
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -138,3 +141,9 @@ DJOSER = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES = (os.path.join(BASE_DIR, 'static'), )
+
+django_heroku.settings(locals())
